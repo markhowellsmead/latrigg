dbd408449842fb33c178101b30b6135c29750eb9
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-07-29 11:52:49 +0200

    Convert post-controls from TOC to useSelect

--- .build/assets/gutenberg/post-controls/main-offset/index.js
+++ .build/assets/gutenberg/post-controls/main-offset/index.js
@@ -1,8 +1,7 @@
 import { _x } from '@wordpress/i18n';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { __experimentalDimensionControl as DimensionControl, ToggleControl } from '@wordpress/components';
-import { compose } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
+import { __experimentalDimensionControl as DimensionControl } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { registerPlugin } from '@wordpress/plugins';
 
 const validPostTypes = ['page'];
@@ -11,32 +10,32 @@ const isValidPostType = function (name) {
 	return validPostTypes.includes(name);
 };
 
-let CustomLayoutPanel = ({ main_offset, post_type, onUpdateMainOffset }) => {
+let CustomLayoutPanel = () => {
+	const post_type = useSelect((select) => {
+		return select('core/editor').getCurrentPostType();
+	});
+
 	if (!isValidPostType(post_type)) {
 		return null;
 	}
 
+	const metaValues = useSelect((select) => {
+		const { getEditedPostAttribute } = select('core/editor');
+		return getEditedPostAttribute('meta');
+	});
+
+	const { main_offset } = metaValues;
+	const { editPost } = useDispatch('core/editor');
+
+	const handleMetaValueChange = (main_offset) => {
+		editPost({ meta: { main_offset } });
+	};
+
 	return (
 		<PluginDocumentSettingPanel title={_x('Custom layout options', 'Editor sidebar panel title', 'latrigg')} initialOpen={true} icon={'invalid-name-no-icon'}>
-			<DimensionControl label={'Main content offset'} value={main_offset} onChange={(main_offset) => onUpdateMainOffset(main_offset)} />
+			<DimensionControl label={'Main content offset'} value={main_offset} onChange={(main_offset) => handleMetaValueChange(main_offset)} />
 		</PluginDocumentSettingPanel>
 	);
 };
 
-const CustomLayoutPanelWithCompose = compose([
-	withSelect((select) => {
-		return {
-			main_offset: select('core/editor').getEditedPostAttribute('meta')['main_offset'],
-			post_type: select('core/editor').getCurrentPostType(),
-		};
-	}),
-	withDispatch((dispatch) => {
-		return {
-			onUpdateMainOffset: (main_offset) => {
-				dispatch('core/editor').editPost({ meta: { main_offset } });
-			},
-		};
-	}),
-])(CustomLayoutPanel);
-
-registerPlugin('latrigg-page-controls', { render: CustomLayoutPanelWithCompose });
+registerPlugin('latrigg-page-controls', { render: CustomLayoutPanel });
