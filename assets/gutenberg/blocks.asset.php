34c337adb52336ccd6ddec2ee8d625520be787cf
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-08-31 23:39:42 +0200

    Move post controls to one file

--- assets/gutenberg/blocks.asset.php
+++ assets/gutenberg/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '0020e741f54fd47aa749');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c5d45ea578dc5579d0cb');
