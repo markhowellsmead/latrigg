e12ba900f524da47e958758cc80251c6de7cc960
Mark Howells-Mead <mark@sayhello.ch>
2024-04-11 23:28:19 +0200

    Custom image sizes are in pt plugin

--- src/Package/Media.php
+++ src/Package/Media.php
@@ -10,53 +10,9 @@ namespace SayHello\Theme\Package;
 class Media
 {
 
-	private $wide_size;
-
 	public function run()
 	{
-
-		$theme_json = json_decode(file_get_contents(get_theme_file_path('theme.json')), true);
-
-		if ((int) ($theme_json['settings']['layout']['wideSize'] ?? false)) {
-			$this->wide_size = (int) $theme_json['settings']['layout']['wideSize'];
-		}
-
-		add_action('after_setup_theme', [$this, 'addImageSizes']);
-		add_filter('image_size_names_choose', [$this, 'selectableImageSizes']);
 		add_filter('embed_defaults', [$this, 'setEmbedDefaults'], 10, 0);
-	}
-
-	/**
-	 * Add image sizes which will be used when uploading images
-	 * wideSize is read in from theme.json.
-	 *
-	 * @return void
-	 */
-	public function addImageSizes()
-	{
-		add_image_size('gutenberg_full', 2560, 9999);
-
-		if ((int) $this->wide_size) {
-			add_image_size('gutenberg_wide', $this->wide_size, 9999);
-		}
-	}
-
-	/**
-	 * Additional selectable image sizes for image size
-	 * selector in e.g. image block
-	 *
-	 * @param array $sizes
-	 * @return array
-	 */
-	public function selectableImageSizes($sizes)
-	{
-		$sizes['gutenberg_full'] = _x('Gestamte breite', 'Custom selectable image size', 'latrigg');
-
-		if ((int) $this->wide_size) {
-			$sizes['gutenberg_wide'] = _x('Weite Breite', 'Custom selectable image size', 'latrigg');
-		}
-
-		return $sizes;
 	}
 
 	/**
