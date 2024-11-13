c50fa30ffff755414ca45908baa7ae8a70ada8c7
Mark Howells-Mead <mark@sayhello.ch>
2024-11-13 16:27:51 +0100

    Revise fancybox behavior

--- .build/assets/scripts/fancybox/index.js
+++ .build/assets/scripts/fancybox/index.js
@@ -1,23 +1,29 @@
 import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/src/Fancybox/Fancybox.scss';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 // Override default behaviour
 Fancybox.defaults.infinite = 0;
 
-document.querySelectorAll('a[target="_self"]:not([download])').forEach((link) => {
+document.querySelectorAll('a[target="_self"]').forEach((link) => {
 	const url = new URL(link.href) || false;
 	if (url instanceof URL) {
 		// preg_match url.pathname for file extension jpg, png, jpeg, gif, svg, webp
-		const regex = /\.(jpg|jpeg|png|gif|svg|webp)$/;
+		const regex = /\.(jpg|jpeg|png|gif|svg|webp|avif)$/;
 		if (regex.test(url.pathname)) {
 			link.removeAttribute('target');
 		}
 	}
 });
 
-Fancybox.bind(
-	"a[href$='.jpg']:not([target]):not([download]), a[href$='.jpeg']:not([target]):not([download]), a[href$='.png']:not([target]):not([download]), a[href$='.gif']:not([target]):not([download]), a[href$='.svg']:not([target]):not([download]), a[href$='.webp']:not([target]):not([download])",
-	{
-		groupAll: true,
-	}
-);
+const suffixes = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp', '.avif'],
+	selector = suffixes.map((suffix) => `a[href$='${suffix}']:not([target]):not([download])`).join(', ');
+
+const options = {
+	groupAll: true,
+	wheel: false,
+	Images: {
+		zoom: false,
+	},
+};
+
+Fancybox.bind(selector, options);
