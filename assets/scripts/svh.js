d824b804ecad9c8eac855b318e7dbede329869f0
Mark Howells-Mead <mark@sayhello.ch>
2025-12-19 14:51:29 +0100

    Modernise SVH

--- assets/scripts/svh.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const e=()=>{document.documentElement.querySelector("body").style.setProperty("--vh",window.innerHeight/100+"px")};e(),window.addEventListener("resize",e),window.addEventListener("orientationchange",e)})();
